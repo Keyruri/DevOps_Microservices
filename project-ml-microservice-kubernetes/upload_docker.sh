d37908a67a603deb107a1028e3e9d556b79c81f0
EC2 Default User <ec2-user@ip-172-31-31-139.ec2.internal>
2023-02-25 06:25:08 +0000

    Files added

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="mmkiruri/udacity-project4"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u mmkiruri
+docker tag service $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
